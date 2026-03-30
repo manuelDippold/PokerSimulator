677638ae3394e75999c6d9838bc5a3e1b4a03014
manuelDippold <manuel_dippold@web.de>
2026-03-30 18:48:56 +0200

    finished first integration test

--- src/test/java/com/yotilla/poker/PokerTableTest.java
+++ src/test/java/com/yotilla/poker/PokerTableTest.java
@@ -1,5 +1,6 @@
 package com.yotilla.poker;
 
+import com.yotilla.poker.card.CardValue;
 import com.yotilla.poker.card.DeckOfCards;
 import com.yotilla.poker.result.GameResult;
 import com.yotilla.poker.result.PokerHandRanking;
@@ -9,6 +10,7 @@ import org.junit.jupiter.api.Test;
 import org.mockito.ArgumentCaptor;
 import org.mockito.invocation.InvocationOnMock;
 
+import java.util.List;
 import java.util.logging.Logger;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -61,8 +63,8 @@ class PokerTableTest {
     void tiedHandsResultInSplitPot() {
         String straight = "6C 5S 7H 9C 8D";
         String straight2 = "6H 5C 7D 9H 8S";
-        String twoPairs = "JH 7D JD AS 7S";
-        String onePair = "JH 7D 2D AS 7S";
+        String twoPairs = "JH 3D JD AS 3S";
+        String onePair = "QH 4D 2D AC 4S";
 
 
         GameResult[] capturedGameResults = new GameResult[1];
@@ -70,8 +72,28 @@ class PokerTableTest {
                 captureGameResult(invocation, capturedGameResults))
                 .when(dealer).determineGameResult(anyList());
 
-        sut.playPoker(new String[]{straight2, straight, twoPairs, onePair});
-        fail("implement me");
+        sut.playPoker(new String[]{straight, straight2, twoPairs, onePair});
+
+        ArgumentCaptor<String> captor = ArgumentCaptor.forClass(String.class);
+        verify(logPrinter, atLeastOnce()).print(captor.capture());
+        assertTrue(captor.getAllValues().stream().anyMatch(s -> s.contains("Players Player 1, Player 2 split the pot.")));
+
+
+        GameResult result = capturedGameResults[0];
+        Player winner = result.getWinner();
+        assertNull(winner);
+
+        List<Player> winners = result.getPotSplit();
+
+        Player winner1 = winners.getFirst();
+        Player winner2 = winners.getLast();
+
+        assertEquals(PokerHandRanking.STRAIGHT, winner1.getPokerHand().ranking());
+        assertEquals(CardValue.NINE, winner1.getPokerHand().rankCards().getFirst());
+
+
+        assertEquals(PokerHandRanking.STRAIGHT, winner2.getPokerHand().ranking());
+        assertEquals(CardValue.NINE, winner2.getPokerHand().rankCards().getFirst());
     }
 
     @Test
